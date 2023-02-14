d2bc019f4486634919590edbbddeccded4178620
Mehrnoush <mehrnoush.vaseghi@gmail.com>
2023-02-14 15:20:34 +0330

    Create retrieves_info_from_a_thumbcacheDB.ps1
    This script retrieves the filename and last access time of all thumbnails in a specified thumbcache database file, using the SQLite module in PowerShell to query the database. To use this script, you would need to replace the thumbcachePath variable with the path to the thumbcache database file you want to analyze.

---
+++ retrieves_info_from_a_thumbcacheDB.ps1
@@ -0,0 +1,21 @@
+$thumbcachePath = "C:\Users\Username\AppData\Local\Microsoft\Windows\Explorer\thumbcache_xxx.db"
+
+# Open the thumbcache database file and retrieve information about the database
+$thumbcache = New-Object System.Data.SQLite.SQLiteConnection("Data Source=$thumbcachePath;Version=3;")
+$thumbcache.Open()
+$thumbcacheCmd = $thumbcache.CreateCommand()
+$thumbcacheCmd.CommandText = "PRAGMA table_info(thumbnail)"
+$thumbcacheTable = $thumbcacheCmd.ExecuteReader()
+
+# Retrieve the filenames and timestamps of all thumbnails in the database
+$thumbcacheCmd.CommandText = "SELECT filename, last_access_time FROM thumbnail"
+$thumbcacheResults = $thumbcacheCmd.ExecuteReader()
+
+# Output the results
+while ($thumbcacheResults.Read())
+{
+    Write-Host "Filename: " $thumbcacheResults.GetValue(0) "Last Access Time: " $thumbcacheResults.GetValue(1)
+}
+
+# Close the thumbcache database file
+$thumbcache.Close()
